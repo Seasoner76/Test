7626f2c3a7388dd3e3027142c2249c164ece110b
Martin Ortner <m.ortner76@googlemail.com>
2023-06-18 18:03:24 +0200

    Erstes Java hinzugefügt

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Held player1 = new Held(46);
+		// player1.getLebenspunkte();
+		//
+		// int schaden = 10;
+		//
+		// player1.setLebenpunkte(schaden);
+		GoogleKonto peter = new GoogleKonto("peter123", "1234");
+
+		peter.setPasswort("1234");
+		
+
+	
+	}
+
+}
